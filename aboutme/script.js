381a4091e40d6527d9e5de895f19624f51bd4104
theycallmerubik <hesam83janmohammady@gmail.com>
2025-02-23 13:24:48 +0330

    Added subpages

---
+++ aboutme/script.js
@@ -0,0 +1,10 @@
+// script.js
+// Example: Add a greeting message in the console
+console.log("Welcome to Hesam's GitHub Profile!");
+
+// Example: Change the background color of the header on click
+const header = document.querySelector('header');
+header.addEventListener('click', () => {
+  header.style.backgroundColor = '#0077cc';
+  header.style.color = '#fff';
+});
